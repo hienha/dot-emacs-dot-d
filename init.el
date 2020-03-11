0d5a60f34ce12113cb08c6ff92fe230f0d2019ae
vcinent <vincent@hienha.com>
2020-03-11 20:09:49 +0800

    accomplish init file splited.

--- init.el
+++ init.el
@@ -9,101 +9,13 @@
 
 (add-to-list 'load-path "~/.emacs.d/lisp")
 
+(require 'init-func)
 (require 'init-packages)
 (require 'init-ui)
+(require 'init-better-defaults)
+(require 'init-org)
+(require 'init-keybindings)
 
+(setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
+(load-file custom-file)
 
-
-;; inhibit ring bell
-(setq ring-bell-function 'ignore)
-
-;; Auto-load file
-(global-auto-revert-mode t)
-
-
-
-(global-linum-mode t)
-
-
-
-
-
-
-(global-set-key "\C-s" 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "<f6>") 'ivy-resume)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "C-h f") 'counsel-describe-function)
-(global-set-key (kbd "C-h v") 'counsel-describe-variable)
-(global-set-key (kbd "C-h l") 'counsel-find-library)
-(global-set-key (kbd "C-h i") 'counsel-info-lookup-symbol)
-(global-set-key (kbd "C-h u") 'counsel-unicode-char)
-(global-set-key (kbd "C-c g") 'counsel-git)
-(global-set-key (kbd "C-c j") 'counsel-git-grep)
-(global-set-key (kbd "C-c k") 'counsel-ag)
-(global-set-key (kbd "C-x l") 'counsel-locate)
-(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
-
-
-
-
-
-
-;; abbrev-mode
-(abbrev-mode t)
-(define-abbrev-table 'global-abbrev-table '(
-					    ;; signature
-					    ("vct" "vincent")
-					    ;; emacs regex
-					    ))
-
-
-
-
-(defun open-my-init-file()
-  (interactive)
-  (find-file "~/.emacs.d/init.el"))
-(global-set-key (kbd "<f2>") 'open-my-init-file)
-(setq make-backup-files nil)
-(setq auto-save-default nil)
-
-(require 'org)
-(setq org-src-fontify-natively t)
-
-(require 'recentf)
-(recentf-mode t)
-(setq recentf-max-menu-items 50)
-(global-set-key "\C-x\ \C-r" 'recentf-open-files)
-
-(delete-selection-mode t)
-
-
-
-(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
-
-
-
-;; org-agenda
-(setq org-agenda-files '("~/org"))
-(global-set-key (kbd "C-c a") 'org-agenda)
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(company-idle-delay 0.08)
- '(company-minimum-prefix-length 2)
- '(exec-path-from-shell-arguments (quote ("-l")))
- '(exec-path-from-shell-check-startup-files nil)
- '(package-selected-packages (quote (company))))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
