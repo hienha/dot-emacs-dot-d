f799f561db9952cf0a74b53b2a688ce0668ff4c5
vcinent <vincent@hienha.com>
2020-03-14 20:58:57 +0800

    org-mode Literate Programming

--- init.el
+++ init.el
@@ -5,17 +5,7 @@
 ;; You may delete these explanatory comments.
 ;;
 ;; Customize settings.
-(package-initialize)
 
-(add-to-list 'load-path "~/.emacs.d/lisp")
-
-(require 'init-func)
-(require 'init-packages)
-(require 'init-ui)
-(require 'init-better-defaults)
-(require 'init-org)
-(require 'init-keybindings)
-
-(setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
-(load-file custom-file)
-
+(require 'org-install)
+(require 'ob-tangle)
+(org-babel-load-file (expand-file-name "vincent.org" user-emacs-directory))
---
+++ vincent.el
@@ -0,0 +1,13 @@
+(package-initialize)
+
+(add-to-list 'load-path "~/.emacs.d/lisp")
+
+(require 'init-func)
+(require 'init-packages)
+(require 'init-ui)
+(require 'init-better-defaults)
+(require 'init-org)
+(require 'init-keybindings)
+
+(setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
+(load-file custom-file)
