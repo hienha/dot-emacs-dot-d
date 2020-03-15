abdf28ff3e6e5563f8aee9b0b7da868265bf5c4f
vcinent <vincent@hienha.com>
2020-03-15 11:46:41 +0800

    Only single quote appears in emacs-lisp mode.

--- lisp/init-better-defaults.el
+++ lisp/init-better-defaults.el
@@ -15,6 +15,12 @@
 (setq auto-save-default nil)
 
 ;; show smarter parentheses
+(define-advice show-paren-function (:around (fn) fix-show-paren-function)
+  "Highlight enclosing parens."
+  (cond ((looking-at-p "\\s(") (funcall fn))
+	(t (save-excursion
+	     (ignore-errors (backward-up-list))
+	     (funcall fn)))))
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
 
 ;; recentf-mode
